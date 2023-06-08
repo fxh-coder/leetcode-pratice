b8d47948c2562c74303920f83787121b2aece93d
fxh <3314965946@qq.com>
2023-06-08 10:23:05 +0800

    feat: complete leetcode symmetric-tree problem

--- src/LC_symmetricTree.java
+++ src/LC_symmetricTree.java
@@ -19,12 +19,15 @@ public class LC_symmetricTree {
     }
 
     public boolean check(TreeNode p, TreeNode q) {
+        // 左右同时为空才正确
         if (p == null && q == null) {
             return true;
         }
+        // 左右有一个不为空就错误
         if (p == null || q == null) {
             return false;
         }
+        // 递归遍历当前节点的值以及子树是否符合条件
         return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
     }
 }
