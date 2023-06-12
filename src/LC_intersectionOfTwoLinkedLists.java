2dfa3e7361ba087f9cd00a5dbf035d1ef628d85c
fxh <3314965946@qq.com>
2023-06-12 16:15:33 +0800

    feat: complete leetcode intersection-of-two-linked-lists problem

---
+++ src/LC_intersectionOfTwoLinkedLists.java
@@ -0,0 +1,29 @@
+public class LC_intersectionOfTwoLinkedLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    /**
+     * 力扣相交链表
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
