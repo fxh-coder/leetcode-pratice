3d9bb5cd4290f27bc8f50ecafc8cb690359f061b
fxh <3314965946@qq.com>
2023-06-17 21:50:45 +0800

    feat: complete leetcode nim-game problem

---
+++ src/Java_Algorithm/LC_nimGame.java
@@ -0,0 +1,12 @@
+package Java_Algorithm;
+
+public class LC_nimGame {
+    /**
+     * 力扣Nim游戏
+     * @param n
+     * @return
+     */
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
