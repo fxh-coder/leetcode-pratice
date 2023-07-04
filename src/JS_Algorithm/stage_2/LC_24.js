7bf5e4096f41e781c6abff0ebea51ffd2e66f896
fxh <3314965946@qq.com>
2023-07-04 09:55:39 +0800

    feat: complete leetcode next-permutation problem use JS

--- src/JS_Algorithm/stage_2/LC_24.js
+++ src/JS_Algorithm/stage_2/LC_24.js
@@ -10,15 +10,20 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    let dummyHead = new ListNode(0, head);
-    let temp = dummyHead;
-    while(temp.next != null && temp.next.next != null) {
-        let node1 = temp.next;
-        let node2 = temp.next.next;
-        temp.next = node2;
-        node1.next = node2.next;
-        node2.next = node1;
-        temp = node1;
-    }
-    return dummyHead.next;
+   let dummy = new ListNode(0);
+   dummy.next = head;
+   let temp = dummy;
+
+   while(temp.next != null && temp.next.next != null) {
+       let node1 = temp.next;
+       let node2 = temp.next.next;
+
+       temp.next = node2;
+       node1.next = node2.next;
+       node2.next = node1;
+
+       temp = node1;
+   }
+
+   return dummy.next;
 };
