43c266872451e7046742b75cd572fbb6b5ee1ba6
fxh-coder <54627251+fxh-coder@users.noreply.github.com>
2023-07-04 22:29:48 +0800

    Add files via upload

---
+++ src/JS_Algorithm/stage_2/LC_26.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 1, j = 1;
+    while (i < nums.length) {
+        if (nums[i] != nums[i - 1]) {
+            nums[j] = nums[i];
+            j++;
+        }
+        i++;
+    }
+    return j;
+};
