6a41fc760d1f759581e6286d4e1904c63dd2d013
fxh-coder <54627251+fxh-coder@users.noreply.github.com>
2023-07-06 21:49:54 +0800

    Add files via upload

---
+++ src/JS_Algorithm/stage_2/LC_2723.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let res = await Promise.all([promise1, promise2]);
+    return res.reduce((total, cur) => {
+       return total + cur;
+    }, 0);
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
