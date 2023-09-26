c4edaf164d8e38140276dfa3fb34f41c65220348
fxh <3314965946@qq.com>
2023-09-26 20:51:05 +0800

    feat: complete leetcode delete-the-middle-node-of-a-linked-list problem use JS

--- src/JS_Algorithm/LC75/LC_2095.js
+++ src/JS_Algorithm/LC75/LC_2095.js
@@ -1,3 +1,4 @@
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -7,52 +8,19 @@
  */
 /**
  * @param {ListNode} head
- * @return {boolean}
+ * @return {ListNode}
  */
-var isPalindrome = function(head) {
-    if (head == null) {
-        return true;
+var deleteMiddle = function(head) {
+    let slow = head, fast = head
+    if (!(head && head.next)) {
+        return null
     }
-
-    const reverseList = (head) => {
-        let prev = null;
-        let curr = head;
-        while (curr != null) {
-            let nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
+    while (fast && fast.next) {
+        fast = fast.next.next
+        if (!(fast && fast.next)) {
+            slow.next = slow.next.next
         }
-        return prev;
+        slow = slow.next
     }
-
-    const endOfFirstHalf = (head) => {
-        let fast = head;
-        let slow = head;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
-
-    // 找到前半部分链表的尾节点并反转后半部分链表
-    let firstHalfEnd = endOfFirstHalf(head);
-    let secondHalfStart = reverseList(firstHalfEnd.next);
-
-    // 判断是否回文
-    let p1 = head;
-    let p2 = secondHalfStart;
-    let result = true;
-    while (result && p2 != null) {
-        if (p1.val != p2.val) {
-            result = false;
-        }
-        p1 = p1.next;
-        p2 = p2.next;
-    }
-
-    // 还原链表并返回结果
-    firstHalfEnd.next = reverseList(secondHalfStart);
-    return result;
-}
+    return head
+}; 
